2f9d14c7a2b0aa555aec2821bc7b488ec9a675b9
Francisco Júnior <franciscoamj1001@gmail.com>
2025-05-02 17:11:06 -0300

    Navbar

---
+++ react-project/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+    content: [
+        './index.html',
+        './src/**/*.{js,jsx,ts,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'neutralSilver': '#F5F7FA',
+                'neutralDGrer': '#4D4D4D',
+                'brandPrimary': '#4CAF4F',
+                'neutralGrey': '#717171',
+                'gray900': '#18191F',
+
+            }
+        },
+    },
+    plugins: [],
+}
